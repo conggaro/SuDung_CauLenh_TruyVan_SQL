efe4907e96b86e8c2e291d813f43a07990c66c3f
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-10-06 15:14:56 +0700

    cap nhat

---
+++ Lưu kết quả truy vấn vào biến có kiểu varchar/lưu kết quả truy vấn vào biến có kiểu varchar.sql
@@ -0,0 +1,32 @@
+﻿create procedure Z_TEST
+as
+begin
+	-- khai báo biến
+	-- để chứa kết quả truy vấn
+	declare @Result varchar(2000)
+	
+	-- khai báo biến bảng
+	declare @MyTable table (gia_tri varchar(2000))
+
+	insert into		@MyTable
+	select			[bảng 2].NAME_VN as 'gia_tri'
+	from			PA_LISTSALARIES as [bảng 1]
+	left join		PA_LISTSAL as [bảng 2]
+	on				[bảng 1].CODE_SAL = [bảng 2].ID
+	where			[bảng 1].OBJ_SAL_ID = 1382
+					and [bảng 1].IS_VISIBLE = 1
+
+
+	-- gán giá trị của câu truy vấn
+	-- vào biến @Result
+	select @Result = COALESCE(@Result + ', ', '') + gia_tri
+	from @MyTable
+
+
+	-- in thử ra màn hình
+	print (@Result)
+end
+
+
+-- thực thi procedure
+execute Z_TEST
