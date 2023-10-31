250b0423622780327a3c30ac5c61281e18a2cc49
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-10-31 20:07:39 +0700

    cap nhat

---
+++ lấy ràng buộc unique và xóa ràng buộc trong sql/cách lấy ra các ràng buộc độc nhất unique trong 1 bảng.sql
@@ -0,0 +1,15 @@
+﻿select *
+from sys.indexes
+where object_id = OBJECT_ID('HU_BANK') and is_unique = 1
+
+
+
+-- bên trong OBJECT_ID()
+-- là tên bảng
+-- ví dụ: tên bảng là "HU_BANK"
+
+
+
+-- xóa ràng buộc
+-- DROP INDEX tên_ràng_buộc ON tên_bảng;
+DROP INDEX IX_HU_BANK__NAME ON HU_BANK;
