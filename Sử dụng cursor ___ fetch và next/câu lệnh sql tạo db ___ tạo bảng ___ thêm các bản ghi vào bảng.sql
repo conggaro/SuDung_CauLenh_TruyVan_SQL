4293e8b39828a3806f611e6a6ad2d68c66b2eea6
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-12-02 18:57:36 +0700

    cap nhat

---
+++ Sử dụng cursor ___ fetch và next/câu lệnh sql tạo db ___ tạo bảng ___ thêm các bản ghi vào bảng.sql
@@ -0,0 +1,35 @@
+﻿CREATE DATABASE [QL_SanPham];
+
+
+USE [QL_SanPham];
+GO
+
+
+CREATE TABLE [SanPham](
+	[Id] [int] IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+	[TenSanPham] [nvarchar](50) NULL,
+	[GiaSanPham] [int] NULL
+);
+GO
+
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham)
+VALUES (N'Máy tính', 500);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham)
+VALUES (N'Điện thoại', 200);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham)
+VALUES (N'Ti-vi', 300);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham)
+VALUES (N'Tủ lạnh', 1000);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham)
+VALUES (N'Điều hòa', 1500);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham)
+VALUES (N'Máy giặt', 1200);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham)
+VALUES (N'Bình nóng lạnh', 900);
---
+++ Sử dụng cursor ___ fetch và next/sử dụng cursor ___ fetch và next.sql
@@ -0,0 +1,31 @@
+﻿--Khai báo biến @id, @title để lưu nội dung đọc
+DECLARE @id int
+DECLARE @name nvarchar(200)
+
+
+DECLARE cursorProduct CURSOR FOR			-- khai báo con trỏ cursorProduct
+SELECT Id, TenSanPham FROM SanPham			-- dữ liệu trỏ tới
+
+
+OPEN cursorProduct							-- Mở con trỏ
+
+
+FETCH NEXT FROM cursorProduct				-- Đọc dòng đầu tiên
+      INTO @id, @name
+
+
+WHILE @@FETCH_STATUS = 0					--vòng lặp WHILE khi đọc Cursor thành công
+BEGIN
+											--In kết quả hoặc thực hiện bất kỳ truy vấn
+											--nào dựa trên kết quả đọc được
+    PRINT 'Id: ' + CAST(@id as nvarchar)
+				 + ', TenSanPham: '
+				 + @name
+
+    FETCH NEXT FROM cursorProduct			-- Đọc dòng tiếp
+          INTO @id, @name
+END
+
+
+CLOSE cursorProduct							-- Đóng Cursor
+DEALLOCATE cursorProduct					-- Giải phóng tài nguyên
