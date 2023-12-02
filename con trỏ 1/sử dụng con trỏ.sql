8cd373ba0e607ae96b22312aca1dcd98ee448027
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-12-02 23:39:16 +0700

    cap nhat

---
+++ con trỏ 1/sử dụng con trỏ.sql
@@ -0,0 +1,25 @@
+﻿declare @id int;
+declare @name nvarchar(50);
+declare @price int;
+
+-- khai báo con trỏ
+declare varCursor cursor for
+select Id, TenSanPham, GiaSanPham
+from SanPham;
+
+-- mở con trỏ
+open varCursor;
+
+-- đọc dòng đầu tiên
+fetch next from varCursor into @id, @name, @price;
+
+-- in ra màn hình
+print	cast(@id as nvarchar(50)) + ', '
+		+ @name + ', '
+		+ cast(@price as nvarchar(50));
+
+-- đóng con trỏ
+close varCursor
+
+-- giải phóng tài nguyên
+deallocate varCursor
---
+++ con trỏ 2/sử dụng con trỏ và vòng lặp while để đọc dữ liệu.sql
@@ -0,0 +1,35 @@
+﻿-- khai báo biến
+declare @id int;
+declare @name nvarchar(100);
+declare @price int;
+
+-- khai báo con trỏ
+declare varCursor cursor for
+select Id, TenSanPham, GiaSanPham
+from SanPham;
+
+-- mở con trỏ
+open varCursor;
+
+-- đọc bản ghi đầu tiên
+fetch	next from varCursor
+		into @id, @name, @price;
+
+-- kiểm tra trạng thái
+while @@fetch_status = 0
+begin
+	-- in kết quả ra màn hình
+	print	cast(@id as nvarchar(50)) + ', '
+			+ @name + ', '
+			+ cast(@price as nvarchar(50));
+
+	-- đọc bản ghi tiếp theo
+	fetch	next from varCursor
+			into @id, @name, @price;
+end
+
+-- đóng con trỏ
+close varCursor;
+
+-- giải phóng tài nguyên
+deallocate varCursor;
