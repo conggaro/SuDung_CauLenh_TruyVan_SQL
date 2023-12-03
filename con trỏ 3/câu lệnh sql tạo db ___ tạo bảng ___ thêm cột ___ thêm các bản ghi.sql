78cc9f8c65bd88aacf596c42fbfa8dd037383cf5
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-12-03 10:15:21 +0700

    cap nhat

---
+++ con trỏ 3/câu lệnh sql tạo db ___ tạo bảng ___ thêm cột ___ thêm các bản ghi.sql
@@ -0,0 +1,41 @@
+﻿CREATE DATABASE [QL_SanPham];
+GO
+
+
+USE [QL_SanPham];
+GO
+
+
+CREATE TABLE [SanPham](
+	[Id] [int] IDENTITY(1, 1) NOT NULL,
+	[TenSanPham] [nvarchar](50) NULL,
+	[GiaSanPham] [int] NULL
+);
+GO
+
+
+ALTER TABLE [SanPham]
+ADD [NgayMuaHang] [date]
+GO
+
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang)
+VALUES (N'Máy tính', 500, '2023/01/31');
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang)
+VALUES (N'Điện thoại', 200, '2022/02/11');
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang)
+VALUES (N'Ti-vi', 300, '2023/03/04');
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang)
+VALUES (N'Tủ lạnh', 1000, '2020/04/19');
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang)
+VALUES (N'Điều hòa', 1500, '2023/08/27');
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang)
+VALUES (N'Máy giặt', 1200, '2019/11/25');
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang)
+VALUES (N'Bình nóng lạnh', 900, '2023/12/01');
---
+++ con trỏ 3/sử dụng con trỏ cursor và fetch ___ next ___ from ___ into.sql
@@ -0,0 +1,47 @@
+﻿-- khai báo biến
+declare @id int;
+declare @name nvarchar(50);
+declare @price int;
+declare @buyDate date;
+
+
+-- khai báo con trỏ
+declare varCursor cursor for
+select Id, TenSanPham, GiaSanPham, NgayMuaHang
+from SanPham;
+
+
+-- mở con trỏ
+open varCursor;
+
+
+-- đọc bản ghi đầu tiên
+fetch	next from varCursor
+		into @id, @name, @price, @buyDate;
+
+
+-- kiểm tra trạng thái đọc
+while @@fetch_status = 0
+begin
+	-- in kết quả ra màn hình
+	print	cast(@id as nvarchar(50)) + ', ' +
+			@name + ', ' +
+			cast(@price as nvarchar(50)) + ', ' +
+			cast(convert(nvarchar(50), @buyDate, 103) as nvarchar(50));
+
+			-- cách 2:
+			-- cast(@buyDate as nvarchar(50))
+			-- thì kết quả là 2023-01-31
+
+	-- đọc bản ghi tiếp theo
+	fetch	next from varCursor
+			into @id, @name, @price, @buyDate;
+end
+
+
+-- đóng con trỏ
+close varCursor;
+
+
+-- giải phóng tài nguyên
+deallocate varCursor;
