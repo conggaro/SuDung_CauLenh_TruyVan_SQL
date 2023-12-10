e4768c5e0f1eca43ea093dc8728aaadd7a6f846f
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-12-10 22:26:31 +0700

    cap nhat

---
+++ chuyển bit sang nvarchar khi select/chuyển bit sang nvarchar khi select.sql
@@ -0,0 +1,25 @@
+﻿-- cách 1:
+-- sử dụng câu lệnh CASE trong câu truy vấn SELECT
+select	Id,
+		TenSanPham,
+		GiaSanPham,
+		NgayMuaHang,
+		case
+			when TrangThai = 1
+			then N'Đang giảm giá'
+			else N'Ngừng giảm giá'
+		end as TrangThaiSanPham
+
+from	SanPham;
+
+
+-- cách 2:
+-- sử dụng hàm IIF (Immediate IF) trong SQL Server
+-- hàm IIF cho phép bạn kiểm tra một điều kiện và trả về kết quả tương ứng
+select	Id,
+		TenSanPham,
+		GiaSanPham,
+		NgayMuaHang,
+		iif(TrangThai = 1, N'Áp dụng', N'Ngừng áp dụng') as TrangThaiSanPham
+
+from	SanPham;
---
+++ chuyển bit sang nvarchar khi select/câu lệnh sql dùng để chuẩn bị dữ liệu.sql
@@ -0,0 +1,46 @@
+﻿CREATE DATABASE [QL_SanPham];
+GO
+
+
+USE [QL_SanPham];
+GO
+
+
+CREATE TABLE [SanPham](
+	[Id] [int] IDENTITY(1, 1) NOT NULL,
+	[TenSanPham] [nvarchar](50) NULL,
+	[GiaSanPham] [int] NULL
+);
+GO
+
+
+ALTER TABLE [SanPham]
+ADD [NgayMuaHang] [date]
+GO
+
+
+ALTER TABLE [SanPham]
+ADD [TrangThai] [bit]
+GO
+
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai)
+VALUES (N'Máy tính', 500, '2023/01/31', 1);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai)
+VALUES (N'Điện thoại', 200, '2022/02/11', 0);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai)
+VALUES (N'Ti-vi', 300, '2023/03/04', 0);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai)
+VALUES (N'Tủ lạnh', 1000, '2020/04/19', 1);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai)
+VALUES (N'Điều hòa', 1500, '2023/08/27', 0);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai)
+VALUES (N'Máy giặt', 1200, '2019/11/25', 1);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai)
+VALUES (N'Bình nóng lạnh', 900, '2023/12/01', 0);
