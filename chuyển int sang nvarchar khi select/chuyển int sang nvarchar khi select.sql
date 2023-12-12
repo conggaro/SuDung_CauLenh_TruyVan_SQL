4fd1239dc6457883c3d1351896d6850ed53f2be0
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-12-12 22:30:36 +0700

    cap nhat

---
+++ chuyển int sang nvarchar khi select/chuyển int sang nvarchar khi select.sql
@@ -0,0 +1,46 @@
+﻿-- cách 1: sử dụng case...when...
+select	Id,
+		TenSanPham,
+		GiaSanPham,
+		NgayMuaHang,
+		TrangThai,
+		case LoaiSanPham
+			when 1 then N'Loại 1'
+			when 2 then N'Loại 2'
+			when 3 then N'Loại 3'
+			else N'Không xác định'
+		end as TenLoaiSanPham
+
+from SanPham
+
+
+-- cách 2: sử dụng hàm iif()
+select	Id,
+		TenSanPham,
+		GiaSanPham,
+		NgayMuaHang,
+		TrangThai,
+		iif(LoaiSanPham = 1, N'Loại 1',
+			iif(LoaiSanPham = 2, N'Loại 2',
+				iif(LoaiSanPham = 3, N'Loại 3', N'Không xác định'))) as TenLoaiSanPham
+
+from SanPham
+
+
+-- Trong cả hai ví dụ trên,
+-- chúng ta sử dụng câu lệnh CASE hoặc hàm IIF
+-- để kiểm tra giá trị của cột "LoaiSanPham".
+
+-- Tùy thuộc vào giá trị của "LoaiSanPham",
+-- chúng ta trả về các chuỗi tương ứng.
+
+-- Nếu giá trị "LoaiSanPham" không phù hợp
+-- với bất kỳ giá trị nào trong CASE hoặc IIF,
+-- chúng ta trả về chuỗi "Không xác định".
+
+-- Lưu ý rằng khi sử dụng hàm IIF,
+-- câu lệnh sẽ trở nên khá dài và phức tạp hơn
+-- nếu có nhiều giá trị để kiểm tra.
+
+-- Trong trường hợp này,
+-- sử dụng câu lệnh CASE sẽ dễ đọc và quản lý hơn.
---
+++ chuyển int sang nvarchar khi select/câu lệnh sql chuẩn bị dữ liệu.sql
@@ -0,0 +1,51 @@
+﻿﻿CREATE DATABASE [QL_SanPham];
+GO
+
+
+USE [QL_SanPham];
+GO
+
+
+CREATE TABLE [SanPham](
+	[Id] [int] IDENTITY(1, 1) NOT NULL,
+	[TenSanPham] [nvarchar](50) NULL,
+	[GiaSanPham] [int] NULL
+);
+GO
+
+
+ALTER TABLE [SanPham]
+ADD [NgayMuaHang] [date]
+GO
+
+
+ALTER TABLE [SanPham]
+ADD [TrangThai] [bit]
+GO
+
+
+ALTER TABLE [SanPham]
+ADD [LoaiSanPham] [int]
+GO
+
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai, LoaiSanPham)
+VALUES (N'Máy tính', 500, '2023/01/31', 1, 1);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai, LoaiSanPham)
+VALUES (N'Điện thoại', 200, '2022/02/11', 0, 2);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai, LoaiSanPham)
+VALUES (N'Ti-vi', 300, '2023/03/04', 0, 3);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai, LoaiSanPham)
+VALUES (N'Tủ lạnh', 1000, '2020/04/19', 1, 3);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai, LoaiSanPham)
+VALUES (N'Điều hòa', 1500, '2023/08/27', 0, 2);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai, LoaiSanPham)
+VALUES (N'Máy giặt', 1200, '2019/11/25', 1, 2);
+
+INSERT INTO SanPham(TenSanPham, GiaSanPham, NgayMuaHang, TrangThai, LoaiSanPham)
+VALUES (N'Bình nóng lạnh', 900, '2023/12/01', 0, 3);
