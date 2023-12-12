850e284398a74d059f46017946491feeaf6b7b68
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-12-12 22:46:58 +0700

    cap nhat

---
+++ dùng điều kiện để in ra kết quả mong muốn khi select ___ case when ___ iif/truy vấn theo điều kiện rồi mới select.sql
@@ -0,0 +1,29 @@
+﻿-- cách 1: sử dụng case...when...
+select	Id,
+		TenSanPham,
+		GiaSanPham,
+		NgayMuaHang,
+		TrangThai,
+		LoaiSanPham,
+		case
+			when GiaSanPham > 1000 then N'Đắt'
+			when GiaSanPham = 1000 then N'Bình thường'
+			when GiaSanPham < 1000 then N'Rẻ'
+			else N'Không xác định'
+		end as NhanXetGiaTienSanPham
+
+from SanPham
+
+
+-- cách 2: sử dụng hàm iif()
+select	Id,
+		TenSanPham,
+		GiaSanPham,
+		NgayMuaHang,
+		TrangThai,
+		LoaiSanPham,
+		iif(GiaSanPham > 1000, N'Đắt',
+			iif(GiaSanPham = 1000, N'Bình thường',
+				iif(GiaSanPham < 1000, N'Rẻ', N'Không xác định'))) as NhanXetGiaTienSanPham
+
+from SanPham
