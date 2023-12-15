859673b477be8b45ceaa0be7d892adab12639b99
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-12-15 17:01:25 +0700

    cap nhat

---
+++ thực hiện phép toán cộng trừ nhân chia khi select/sử dụng phép  toán và select.sql
@@ -0,0 +1,45 @@
+﻿-- thực hiện phép cộng
+select	Id,
+		TenSanPham,
+		GiaSanPham + 10 as GiaSanPham_PhepToanCong,
+		NgayMuaHang,
+		TrangThai,
+		LoaiSanPham
+
+from SanPham
+
+
+
+-- thực hiện phép trừ
+select	Id,
+		TenSanPham,
+		GiaSanPham - 10 as GiaSanPham_PhepToanTru,
+		NgayMuaHang,
+		TrangThai,
+		LoaiSanPham
+
+from SanPham
+
+
+
+-- thực hiện phép nhân
+select	Id,
+		TenSanPham,
+		GiaSanPham * 1000 as GiaSanPham_PhepToanNhan,
+		NgayMuaHang,
+		TrangThai,
+		LoaiSanPham
+
+from SanPham
+
+
+
+-- thực hiện phép chia
+select	Id,
+		TenSanPham,
+		GiaSanPham / 100 as GiaSanPham_PhepToanChia,
+		NgayMuaHang,
+		TrangThai,
+		LoaiSanPham
+
+from SanPham
