1e88bd66ad750d08c6a16f73fe1f4b3f2e5ef701
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-12-24 11:06:52 +0700

    cap nhat

---
+++ sử dụng PIVOT/sử dụng PIVOT để xoay trục.sql
@@ -0,0 +1,68 @@
+﻿-- cách 1:
+-- sử dụng pivot để xoay trục
+select *
+from (
+	select		b1.MaSV_FK as MaSinhVien,
+				
+				-- nhóm các bản ghi theo mã sinh viên
+				-- và tên sinh viên
+				-- (nếu bạn select Id của bảng KetQua thì kết quả xoay trục sẽ bị hỏng)
+				b2.TenSV,
+
+				-- dùng để xoay trục
+				-- nó còn nhóm luôn cả mã học kỳ
+				-- nhưng mã học kỳ lại rải thành các cột
+				b3.MaHK as MaHocKy,
+				
+				-- giá trị ăn theo, điểm trung bình sẽ ăn theo học kỳ
+				b1.DiemTB
+
+	from		KetQua b1
+	inner join	SinhVien b2
+	on			b1.MaSV_FK = b2.MaSV
+	inner join	HocKy b3
+	on			b1.MaHK_FK = b3.MaHK
+) as BangTam
+pivot (min(DiemTB) for MaHocKy in ([HK1], [HK2], [HK3], [HK4])) as BangXoayTruc
+
+
+
+-- thêm từ khóa "go"
+-- để biên dịch tiếp câu lệnh bên dưới
+go
+
+
+
+-- cách 2:
+-- sử dụng with để lưu cả câu truy vấn select
+-- vào vùng nhớ có tên TempData
+with TempData as (
+	select		b1.MaSV_FK as MaSinhVien,
+				b2.TenSV,
+				b3.MaHK as MaHocKy,
+				b1.DiemTB
+
+	from		KetQua b1
+	inner join	SinhVien b2
+	on			b1.MaSV_FK = b2.MaSV
+	inner join	HocKy b3
+	on			b1.MaHK_FK = b3.MaHK
+)
+
+-- sử dụng pivot
+select *
+from TempData
+pivot (
+	min(DiemTB) for MaHocKy
+	in ([HK1], [HK2], [HK3], [HK4])
+) as TablePivot
+
+
+
+-- ghi chú:
+-- Trong SQL Server, từ khóa WITH
+-- thường được sử dụng để định nghĩa một Common Table Expression (CTE)
+-- chúng ta có thể tạm dịch là "Biểu thức Bảng Chung"
+-- CTE là một cấu trúc truy vấn tạm thời
+-- và khi sử dụng WITH, bạn có thể lưu kết quả của một truy vấn tạm thời
+-- vào một tên cụ thể.
