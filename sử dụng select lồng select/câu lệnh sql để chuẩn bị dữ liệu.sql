f592c1e9009dac4e13bb8bd5affafa4bc6cdfe83
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-12-25 08:57:43 +0700

    cap nhat

---
+++ sử dụng select lồng select/câu lệnh sql để chuẩn bị dữ liệu.sql
@@ -0,0 +1,74 @@
+﻿﻿-- tạo cơ sở dữ liệu
+create database QL_SinhVien;
+go
+
+
+-- sử dụng db
+use QL_SinhVien;
+go
+
+
+-- tạo bảng sinh viên
+create table SinhVien (
+	Id int identity(1, 1) primary key,
+	MaSV nvarchar(100),
+	TenSV nvarchar(100)
+);
+
+
+-- tạo bảng học kỳ
+create table HocKy (
+	Id int identity(1, 1) primary key,
+	MaHK nvarchar(100),
+	TenHK nvarchar(100)
+);
+
+
+-- tạo bảng kết quả
+create table KetQua (
+	Id int identity(1, 1) primary key,
+	MaSV_FK nvarchar(100),
+	MaHK_FK nvarchar(100),
+	DiemTB float
+);
+
+
+-- thêm bản ghi vào bảng SinhVien
+insert into SinhVien (MaSV, TenSV)
+values (N'SV001', N'Nguyễn Văn A');
+
+insert into SinhVien (MaSV, TenSV)
+values (N'SV002', N'Nguyễn Văn B');
+
+insert into SinhVien (MaSV, TenSV)
+values (N'SV003', N'Nguyễn Văn C');
+
+
+-- thêm bản ghi vào bảng HocKy
+insert into HocKy (MaHK, TenHK)
+values
+(N'HK1', N'Học kỳ 1'),
+(N'HK2', N'Học kỳ 2'),
+(N'HK3', N'Học kỳ 3'),
+(N'HK4', N'Học kỳ 4'),
+(N'HK5', N'Học kỳ 5'),
+(N'HK6', N'Học kỳ 6'),
+(N'HK7', N'Học kỳ 7'),
+(N'HK8', N'Học kỳ 8');
+
+
+-- thêm bản ghi vào bảng KetQua
+insert into KetQua (MaSV_FK, MaHK_FK, DiemTB)
+values
+(N'SV001', N'HK1', 9.9),
+(N'SV002', N'HK1', 8.2),
+(N'SV003', N'HK1', 10),
+(N'SV001', N'HK2', 8.5),
+(N'SV002', N'HK2', 9.6),
+(N'SV003', N'HK2', 8.4),
+(N'SV001', N'HK3', 9.7),
+(N'SV002', N'HK3', 10),
+(N'SV003', N'HK3', 8.7),
+(N'SV001', N'HK4', 10),
+(N'SV002', N'HK4', 7.1),
+(N'SV003', N'HK4', 9.8);
---
+++ sử dụng select lồng select/select lồng select.sql
@@ -0,0 +1,24 @@
+﻿-- sử dụng select lồng select
+select		b3.Id as N'Id (Kết quả)',
+			b1.MaSV as N'Mã SV',
+			b1.TenSV as N'Tên SV',
+			b2.MaHK as N'Mã HK',
+			b2.TenHK as N'Tên HK',
+			b3.DiemTB as N'Điểm trung bình'
+
+from
+(
+	select *
+	from SinhVien
+) as b1,
+(
+	select *
+	from HocKy
+) as b2,
+(
+	select *
+	from KetQua
+) as b3
+
+where		b1.MaSV = b3.MaSV_FK
+			and b2.MaHK = b3.MaHK_FK;
