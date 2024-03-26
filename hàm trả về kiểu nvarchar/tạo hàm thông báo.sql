2de9b131f64e4196a98b3a5acfd03f8a8e679cfd
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2024-03-26 11:07:55 +0700

    cap nhat

---
+++ hàm trả về kiểu nvarchar/tạo hàm thông báo.sql
@@ -0,0 +1,30 @@
+﻿-- tạo hàm trả về kiểu nvarchar(255)
+create function ThongBao (
+	@code nvarchar(255),
+	@name nvarchar(255)
+)
+returns nvarchar(255)
+as
+begin
+	declare @result nvarchar(255);
+
+	set @result = @code + ' - ' + @name;
+
+	return @result;
+end;
+
+
+
+--gọi hàm ThongBao
+declare @thongBao nvarchar(255) = dbo.ThongBao(N'A001', N'Nguyễn Văn A');
+
+-- in ra màn hình
+select @thongBao as Thong_bao;
+
+-- in ra cửa sổ Messages
+print @thongBao;
+
+
+
+-- xóa hàm
+drop function if exists dbo.ThongBao;
