e71d587632deaa2abc46c47aa06b94e83d5b9ef0
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2024-03-26 10:33:28 +0700

    cap nhat

---
+++ hàm tính tổng/hàm tính tổng _ kiểu trả về của hàm là int.sql
@@ -0,0 +1,22 @@
+﻿-- tạo hàm tính tổng
+create function TinhTong (
+    @so1 int,
+    @so2 int
+)
+returns int
+as
+begin
+    declare @tong int;
+
+    set @tong = @so1 + @so2;
+
+    return @tong;
+end;
+
+
+-- gọi hàm tính tổng
+select dbo.TinhTong(5, 7) AS Tong;
+
+
+-- câu lệnh xóa hàm
+drop function if exists dbo.TinhTong;
