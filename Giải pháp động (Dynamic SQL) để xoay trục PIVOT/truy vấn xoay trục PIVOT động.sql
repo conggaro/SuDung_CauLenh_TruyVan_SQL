eecba079a1f3986d397048bafbaf485cac3e280e
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2024-10-13 12:06:49 +0700

    cap nhat

---
+++ Giải pháp động (Dynamic SQL) để xoay trục PIVOT/truy vấn xoay trục PIVOT động.sql
@@ -0,0 +1,37 @@
+﻿-- Khai báo biến @query để chứa truy vấn động
+declare @query as nvarchar(max);
+
+-- Khai báo biến @cols để chứa danh sách cột
+declare @cols as nvarchar(max);
+
+-- Lấy danh sách các giá trị mã học kỳ động
+select @cols = STRING_AGG(QUOTENAME(CODE), ',')
+from CODE_HK;
+
+-- Tạo câu truy vấn động cho pivot
+set @query = '
+select *
+from (
+	select		b1.MaSV_FK as MaSinhVien,
+				b2.TenSV,
+				b3.MaHK as MaHocKy,
+				b1.DiemTB
+	from		KetQua b1
+	inner join	SinhVien b2
+	on			b1.MaSV_FK = b2.MaSV
+	inner join	HocKy b3
+	on			b1.MaHK_FK = b3.MaHK
+) as BangTam
+pivot (
+	min(BangTam.DiemTB)
+	for BangTam.MaHocKy in (' + @cols + ')
+) as BangXoayTruc';
+
+-- In ra biến @cols
+print @cols;
+
+-- In ra câu truy vấn động
+print @query;
+
+-- Thực thi câu truy vấn động
+exec sp_executesql @query;
---
+++ Giải pháp động (Dynamic SQL) để xoay trục PIVOT/truy vấn xoay trục và có sắp xếp.sql
@@ -0,0 +1,27 @@
+﻿select *
+from (
+	select		b1.MaSV_FK as MaSinhVien,
+				
+				-- nhóm các bản ghi theo mã sinh viên
+				-- và tên sinh viên
+				-- (nếu bạn select Id của bảng KetQua thì kết quả xoay trục sẽ bị hỏng)
+				b2.TenSV,
+
+				-- dùng để xoay trục
+				-- nó còn nhóm luôn cả mã học kỳ
+				-- nhưng mã học kỳ lại rải thành các cột
+				b3.MaHK as MaHocKy,
+				
+				-- giá trị ăn theo, điểm trung bình sẽ ăn theo học kỳ
+				b1.DiemTB
+
+	from		KetQua b1
+	inner join	SinhVien b2
+	on			b1.MaSV_FK = b2.MaSV
+	inner join	HocKy b3
+	on			b1.MaHK_FK = b3.MaHK
+) as BangTam
+pivot (min(DiemTB) for MaHocKy in ([HK1], [HK2], [HK3], [HK4])) as BangXoayTruc
+
+-- sắp xếp theo cột TenSV
+order by TenSV desc;
